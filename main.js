34287bc6cac8701c6a891a972b14cf03dcfa44c6
ruslan <parhomenkogm@gmail.com>
2024-05-16 23:09:04 +0300

    add comunication html/css, history html/css

---
+++ main.js
@@ -0,0 +1,16 @@
+// import './style.css'
+import './src/css/index.css'
+
+import htmlComunication360 from './src/component/comunication360.html?raw';
+import mesagingHistory from './src/component/mesaging-history.html?raw';
+
+
+
+
+
+document.querySelector('#app').innerHTML = `
+${htmlComunication360}
+${mesagingHistory}
+  
+`
+
