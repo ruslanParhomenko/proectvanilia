3bc85ce0c8d3b21b5d6962f864d33483aa21fa3c
ruslan <parhomenkogm@gmail.com>
2024-05-21 22:35:47 +0300

    add new function open list/ component/ vector transform

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-import { openListBroadcast } from "./src/componentJS/openListBroadcast";
 
-const openHistory = ['#communication360-messaging-history','#section-mesaging','mesaging-history-item']
 
-document.querySelector('.navigation-drawer').addEventListener('click',(e) => {
+const historySection = document.querySelector('#section-mesaging')
+const messagingUl = document.querySelector('#messaging-list')
+
+const listUlBroadcoast = document.querySelectorAll('.nav-item-prime')
+
+const openList = 'open-list'
+const vectorUpDown = 'transform-vector'
+const dataA = 'data-a'
+const dataLi = 'data-li'
+
+listUlBroadcoast.forEach(el => console.log(el.addEventListener('click', (e)=> {
     e.preventDefault()
-    e.stopPropagation()
+    let event = e.target
+    
+    if((+event.getAttribute(dataLi) || +event.getAttribute(dataA)) === 2) {
+        document.querySelector('#vector-2').classList.toggle(vectorUpDown)
+        messagingUl.classList.toggle(openList)
+    }
+    if((+event.getAttribute(dataLi) || +event.getAttribute(dataA)) === 24 ) {
+        historySection.classList.toggle(openList)
+    }
 
-    openListBroadcast(e.target,...openHistory)
-
-})
-
-const openMessaging = ['#communication360-messaging','#messaging-list','messaging-list']
-document.querySelector('.navigation-drawer').addEventListener('click', (e) => {
-
-    openListBroadcast(e.target,...openMessaging)
+})))
 
 
-})
+
